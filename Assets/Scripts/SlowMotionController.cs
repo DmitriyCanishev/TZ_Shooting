c45b01112ee6780cd03ec132f866f53523aa6153
Dmitriy Canishev <canishev.dima2019@gmail.com>
2020-08-01 14:56:48 +0300

    Add SlowMotion script

---
+++ Assets/Scripts/SlowMotionController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SlowMotionController
+{
+    public float slowMoValue = 0.07f;
+    public float slowMoDuration = 2f;
+
+    void Update()
+    {
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0.0f, 1.0f);
+        Time.fixedDeltaTime = Mathf.Clamp(Time.timeScale, 0.0f, 0.02f);
+        Time.timeScale += (1.0f / slowMoDuration) * Time.unscaledDeltaTime;
+        Time.fixedDeltaTime += (0.02f / slowMoDuration) * Time.unscaledDeltaTime;
+    }
+
+    public void SlowMotion()
+    {
+        Time.timeScale = slowMoValue;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    }
+}
+
